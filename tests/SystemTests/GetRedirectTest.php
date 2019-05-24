e9e267aee3c1152bec4254c4a74b4fcec98c4985
Thomas Alrek <thomas@alrek.no>
2019-05-24 14:44:45 +0200

    Remove unecessary require

--- tests/SystemTests/GetRedirectTest.php
+++ tests/SystemTests/GetRedirectTest.php
@@ -10,7 +10,6 @@ final class GetRedirectTest extends TestCase
    */
   protected function setUp(): void
   {
-    require_once(__DIR__ . '/../mocks/NF.php');
     require_once(__DIR__ . '/../../src/functions/common/functions_system.php');
   }
 
--- tests/SystemTests/GetSettingTest.php
+++ tests/SystemTests/GetSettingTest.php
@@ -9,7 +9,6 @@ final class GetSettingTest extends TestCase
    */
   protected function setUp(): void
   {
-    require_once(__DIR__ . '/../mocks/NF.php');
     require_once(__DIR__ . '/../../src/functions/common/functions_system.php');
   }
 
