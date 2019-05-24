9a387e5377bf135fc1ae704266778795e06e7b24
Thomas Alrek <thomas@alrek.no>
2019-05-24 14:44:57 +0200

    Mock PHP console

--- tests/mocks/NF.php
+++ tests/mocks/NF.php
@@ -3,7 +3,10 @@
 require_once(__DIR__ . '/Cache.php');
 require_once(__DIR__ . '/Guzzle.php');
 require_once(__DIR__ . '/Site.php');
+require_once(__DIR__ . '/PHPConsole.php');
 
 NF::$capi = new MockGuzzle();
 NF::$cache = new MockCache();
 NF::$site = new MockSite();
+NF::$console = new MockPHPConsole();
+NF::$site_root = __DIR__ . '/project/';
