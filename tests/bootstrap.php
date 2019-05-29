3a28ff907642f364ccff2d95ae7de2abb5363357
Thomas Alrek <thomas@alrek.no>
2019-05-29 17:07:13 +0200

    Handle warnings like Netflex

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,5 +1,7 @@
 <?php
 
+error_reporting(E_ERROR | E_WARNING | E_PARSE);
+
 require_once(__DIR__ . '/mocks/NF.php');
 require_once(__DIR__ . '/mocks/helpers.php');
 require_once(__DIR__ . '/drivers/TextDriver.php');
