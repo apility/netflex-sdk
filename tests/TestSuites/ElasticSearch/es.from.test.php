6b513964ad66dcde0847852390e995fd1cab30c1
Thomas Alrek <thomas@alrek.no>
2019-08-08 14:39:58 +0200

    Implement testsuite for ElasticSearch

---
+++ tests/TestSuites/ElasticSearch/es.from.test.php
@@ -0,0 +1,30 @@
+<?php
+
+use Netflex\Site\ElasticSearch;
+use PHPUnit\Framework\TestCase;
+use Spatie\Snapshots\MatchesSnapshots;
+use Spatie\Snapshots\Drivers\JsonDriver;
+
+final class ElasticSearchFromTest extends TestCase
+{
+  use MatchesSnapshots;
+
+  /**
+   * Call this template method before each test method is run.
+   */
+  protected function setUp(): void
+  {
+    require_once('src/Netflex/Site/ElasticSearch.php');
+  }
+
+  public function testDirectoryOutputsMatchesSnapshot(): void
+  {
+    $search = new ElasticSearch;
+    $search->from(10);
+
+    $this->assertMatchesSnapshot(
+      $search->buildQuery(),
+      new JsonDriver
+    );
+  }
+}
