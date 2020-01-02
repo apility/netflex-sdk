9df5948e4c10d32485c0fc13c47c3d787f9ee397
Olav Øyen <olav@apility.no>
2020-01-02 20:08:25 +0100

    Update cache key to match the key used in find() and all()

--- src/model/Structure.php
+++ src/model/Structure.php
@@ -158,7 +158,7 @@ abstract class Structure implements ArrayAccess, Serializable, JsonSerializable
     }
 
     $this->_modified = [];
-    NF::$cache->save('builder_structures_entry_' . $this->id, serialize($this->attributes));
+    NF::$cache->save('entry/' . $this->id, serialize($this->attributes));
 
     static::performHookOn($this, 'saved');
     return $this;
